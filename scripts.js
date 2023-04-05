7dc56eb591648f6e667098a19c3bbfd3e68bd0ae
brybrywithayy <brybrywithayy@gmail.com>
2023-04-05 16:04:44 -0400

    Add function and loop to generate divs and append to parent container in the DOM

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
 /*
  * Etch-a-Sketch
  * 
- * inserting 16x16 grid of divs into the DOM for the base
+ * Insert 16x16 grid of divs into the DOM for the base
+ * 
  * 
  */
+
+// append 16x16 grid of divs onto an existing div
+const container = document.querySelector("#gridContainer");
+
+// function creates a div and appends to the parent container
+function createDiv() {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("divGrid");
+    container.appendChild(newDiv);
+}
+
+// loop iterates and creates rows and columns of divs
+for (i=0; i<16; i++) {
+    for (j=0; j<16; j++) {
+        createDiv();
+    }
+}
