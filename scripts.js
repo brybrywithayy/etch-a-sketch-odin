aa284c114f0ad134da231711294cf144ba7fbc4c
brybrywithayy <brybrywithayy@gmail.com>
2023-04-05 18:01:50 -0400

    Add mouseover and mousedown boolean functions and function to change background color of element when mouse click occurs

--- scripts.js
+++ scripts.js
@@ -2,11 +2,15 @@
  * Etch-a-Sketch
  * 
  * Insert 16x16 grid of divs into the DOM for the base
- * 
- * 
+ * Allow user to repaint canvas with custom grid size
+ * Have grids signify that they have been painted on with background color change
+ *  
  */
 
-// append 16x16 grid of divs onto an existing div
+// default canvas size on page load
+const DEFAULT_CANVAS_SIZE = 16;
+
+// append grid of divs onto an existing div
 const gridCanvas = document.querySelector("#gridContainer");
 
 // function creates a div and appends to the parent container
@@ -21,6 +25,7 @@ function createDiv() {
 
 // validate user selected size
 function validateUserSize(userGridSize) {
+    
     if (userGridSize > 100) {
         return 100;
     } else if (userGridSize <= 0) {
@@ -37,4 +42,40 @@ function createGrid(userGridSize) {
     for (i=0; i< gridSize * gridSize; i++) {
         createDiv();
     }
+
+    // reset the canvas row-column alignment
+    gridCanvas.setAttribute("style", `display: grid; grid-template-columns: repeat(${gridSize}, auto`);
 }
+
+// determine if grid is mouseover'ed
+function isMouseover(e) {
+    if (e.type === "mouseover") {
+        console.log("mouseover");
+        return true;
+    } else {
+        return false;
+    }
+}
+// determine if a grid is mousedown'ed
+// CURRENTLY NOT WORKING
+function isMousedown(e) {
+    if (e.type === "mousedown") {
+        console.log("mousedown");
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// takes an event from event listeners attached to each grid in the canvas
+function changeBackgroundColor(e) {
+    
+    if (isMouseover(e) === true) {
+        e.classList.add("gridBoxElementPainted");
+        // if (isMousedown(e) === true) {
+        //     e.classList.add("gridBoxElementPainted");
+        // } 
+    }
+}
+
+createGrid(DEFAULT_CANVAS_SIZE);
