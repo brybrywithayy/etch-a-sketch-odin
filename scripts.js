5558b3a0acd7d4ed21ea5e6485629289eee52d49
brybrywithayy <brybrywithayy@gmail.com>
2023-04-07 13:05:03 -0400

    Add button and field in html to resize div grid canvas

--- scripts.js
+++ scripts.js
@@ -20,8 +20,11 @@ function createDiv() {
     newBox.setAttribute("draggable", "false"); // still having major problems with mousedown not triggering
     // listen for mouseover or mousedown - paint conditions
     newBox.addEventListener("mousedown", changeBackgroundColor);
-    newBox.addEventListener("mouseover", changeBackgroundColor);
     gridCanvas.appendChild(newBox);
+}
+
+function destroyGrids() {
+    gridCanvas.textContent = "";
 }
 
 // validate user selected size
@@ -38,6 +41,8 @@ function validateUserSize(userGridSize) {
 
 // Don't need to loop for rows and columns, just get the total number
 function createGrid(userGridSize) {
+    destroyGrids();
+
     let gridSize = validateUserSize(userGridSize);
 
     for (i=0; i< gridSize * gridSize; i++) {
@@ -48,33 +53,14 @@ function createGrid(userGridSize) {
     gridCanvas.setAttribute("style", `display: grid; grid-template-columns: repeat(${gridSize}, auto`);
 }
 
-// determine if grid is mouseover'ed
-function isMouseover(e) {
-    if (e.type === "mouseover") {
-        console.log("mouseover");
-        return true;
-    } else {
-        return false;
-    }
-}
-// determine if a grid is mousedown'ed
-// CURRENTLY NOT WORKING
-function isMousedown(e) {
-    if (e.type === "mousedown") {
-        console.log("mousedown");
-        return true;
-    } else {
-        return false;
-    }
-}
-
 // takes an event from event listeners attached to each grid in the canvas
 function changeBackgroundColor(e) {
     
-    if (isMouseover(e) === true) {
-        if (isMousedown(e) === true) {
-            e.classList.add("gridBoxElementPainted");
-        } 
+    if (e.type === "mouseover") {
+        if (e.onmousedown) {
+            e.target.classList.add("gridBoxElementPainted");
+            e.target.classList.remove("gridBoxElement");
+        }
     }
 }
 
