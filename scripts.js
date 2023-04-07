feb4112b88eeda25c89f0f63ce0837c6827cda42
brybrywithayy <brybrywithayy@gmail.com>
2023-04-07 14:34:30 -0400

    Add somethings, broke a lot more

--- scripts.js
+++ scripts.js
@@ -9,33 +9,44 @@
 
 // default canvas size on page load
 const DEFAULT_CANVAS_SIZE = 16;
+const DEFAULT_COLOR = "#000000";
 
 // append grid of divs onto an existing div
 const gridCanvas = document.querySelector("#gridContainer");
 
+// button event listeners
+// document.getElementById("blackBtn").addEventListener("click", changeBackgroundColor);
+// document.getElementById("randomBtn").addEventListener("click", changeBackgroundColor);
+document.getElementById("resetBtn").addEventListener("click", defaultGrid);
+
+
 // function creates a div and appends to the parent container
 function createDiv() {
+    console.log("Generating divs...");
     const newBox = document.createElement("div");
     newBox.classList.add("gridBoxElement");
     newBox.setAttribute("draggable", "false"); // still having major problems with mousedown not triggering
-    // listen for mouseover or mousedown - paint conditions
+    // listen for mouseover - paint conditions
     newBox.addEventListener("mouseover", changeBackgroundColor);
-    // newBox.addEventListener("mouseover", changeBackgroundColor);
     gridCanvas.appendChild(newBox);
 }
 
 function destroyGrids() {
     gridCanvas.textContent = "";
+    console.log("Old divs erased!");
 }
 
 // validate user selected size
 function validateUserSize(userGridSize) {
     
     if (userGridSize > 100) {
+        console.log("User entered " + userGridSize + ", Setting to 100");
         return 100;
     } else if (userGridSize <= 0) {
+        console.log("User entered " + userGridSize + ", Setting to 1");
         return 1;
     } else {
+        console.log("Setting board to " + userGridSize);
         return userGridSize;
     }
 }
@@ -46,6 +57,8 @@ function createGrid(userGridSize) {
 
     let gridSize = validateUserSize(userGridSize);
 
+    console.log("Creating " + gridSize * gridSize + " divs");
+
     for (i=0; i< gridSize * gridSize; i++) {
         createDiv();
     }
@@ -54,11 +67,32 @@ function createGrid(userGridSize) {
     gridCanvas.setAttribute("style", `display: grid; grid-template-columns: repeat(${gridSize}, auto`);
 }
 
+// DON'T WORK CURRENTLY
+// function randomize() {
+//     return Math.floor(Math.random() * 256);
+// }
+
+// function setColorBlack(e) {
+//     e.target.style.backgroundColor = DEFAULT_COLOR;
+// }
+
+// function setColorRandom(e) {
+//     let valueR = randomize();
+//     let valueG = randomize();
+//     let valueB = randomize();
+
+//     let valueRGB = `rgb(${valueR}, ${valueB}, ${valueG})`;
+// }
+
 // takes an event from event listeners attached to each grid in the canvas
 function changeBackgroundColor(e) {
-    
-    e.target.classList.add("gridBoxElementPainted");
-    e.target.classList.remove("gridBoxElement");
+    e.target.classList.add("gridBoxElementBlack");
 }
 
-createGrid(DEFAULT_CANVAS_SIZE);
+// for reset button implementation
+function defaultGrid() {
+    createGrid(DEFAULT_CANVAS_SIZE);
+}
+
+// set initial div grid up
+defaultGrid(DEFAULT_CANVAS_SIZE);
